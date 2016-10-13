c3da0c3f884bc166cd679fe20400faff90702422
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-10-13 20:00:19 +0300

    Added 1.3: Add search by tag The module should return all images by tag sorted by their date of creation in descending order: instanodeDb.findByTag(‘cat’)

--- 01.MongoDB and Mongoose/homework/index.js
+++ 01.MongoDB and Mongoose/homework/index.js
@@ -1,4 +1,3 @@
-let mongodb = require('mongodb')
 let mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 let connection = 'mongodb://localhost:27017/instanode-db'
@@ -8,6 +7,8 @@ mongoose
   .connect(connection)
   .then(() => {
     console.log('MongoDB up and running!')
-    instanodeDb.saveImage({ url: 'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg', date: new Date(), description: 'such cat much wow', tags: ['cat', 'kitty', 'cute', 'catstagram'] })
-    
+    //  instanodeDb.saveImage({ url: 'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg', date: new Date(), description: 'such cat much wow', tags: ['cat', 'kitty', 'cute', 'catstagram'] })
+    instanodeDb.findByTag('cat', (foundImages) => {
+      console.log(foundImages)
+    })
   })
--- 01.MongoDB and Mongoose/homework/instanode-db.js
+++ 01.MongoDB and Mongoose/homework/instanode-db.js
@@ -1,4 +1,6 @@
 let mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+let _ = require('lodash')
 let imageSchema = require('./create-image')
 
 function saveImage (imageInfo) {
@@ -9,11 +11,29 @@ function saveImage (imageInfo) {
     description: imageInfo.description,
     tags: imageInfo.tags
   }).save()
-  imageSchema.find().then(console.log)
+  imageSchema.findByT
+}
+
+function findByTag (tag, callback) {
+  // this function searches the database for images that have the corresponding tag
+  console.log('Trying to find images with the tag ' + tag + ' in them.')
+  imageSchema.find({}).then(function (docs) {
+    let imagesWithTag = []  // array we'll be returning
+
+    for (let idx in docs) {
+      let image = docs[idx]._doc
+
+      if (_.includes(image.tags, tag)) {  // if the image has the tag
+        imagesWithTag.push(image)
+      }
+    }
+
+    callback(imagesWithTag)  // return the images
+  })
 }
 
 function saveTag (tagInfo) {
   console.log('well what the fuck')
 }
 
-module.exports = {saveImage: saveImage, saveTag: saveTag}
+module.exports = {saveImage: saveImage, saveTag: saveTag, findByTag: findByTag}
