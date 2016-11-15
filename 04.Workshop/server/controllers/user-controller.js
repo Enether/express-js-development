6762f3bb82cae1b97a7b9f069dbb39c2c1e31c3f
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-11-15 20:17:55 +0200

    Added validation of username and password for registering/logging in

--- 04.Workshop/server/controllers/user-controller.js
+++ 04.Workshop/server/controllers/user-controller.js
@@ -3,6 +3,17 @@ const encryption = require('../utilities/encryption')
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
 
+function usernameIsValid(username) {
+  // username should only have letters and numbers
+  let pattern = /^[A-Za-z0-9]{3,20}$/
+  return pattern.test(username)
+}
+function passwordIsValid(username) {
+  // username should only have letters and numbers
+  let pattern = /^[A-Za-z0-9]{4,20}$/
+  return pattern.test(username)
+}
+
 module.exports = {
   showRegister: (req, res) => {
     res.render('user/register', { regArgs: {} })
@@ -31,18 +42,27 @@ module.exports = {
 
   register: (req, res) => {
     let user = req.body
-
+    let usernameCandidate = user.username
+    let passwordCandidate = user.password
+    if (!usernameIsValid(usernameCandidate)) {
+      res.render('user/register', { regArgs: { username: usernameCandidate }, nonFatalError: 'Username can only contain letters and numbers. It should also have minimum length of 3 and maximum length of 20!' })
+      return
+    } else if (!passwordIsValid(passwordCandidate)) {
+      res.render('user/register', { regArgs: { username: usernameCandidate }, nonFatalError: 'Password can only contain letters and numbers. It also has a minimum length of 4 and a maximum length of 20!' })
+      return
+    }
+    // VALIDATE!
     if (user.password !== user.password_confirm) {
       // error
-      res.render('user/register', { regArgs: { username: user.username }, nonFatalError: 'Your passwords do not match :@' })
+      res.render('user/register', { regArgs: { username: usernameCandidate }, nonFatalError: 'Your passwords do not match :@' })
       return
     } else {
       // check same username
       User
-        .find({ username: user.username })
+        .find({ username: usernameCandidate })
         .then(user => {
           if (user) {
-            res.render('user/register', { regArgs: { username: user.username }, nonFatalError: 'Such an username already exists!' })
+            res.render('user/register', { regArgs: { username: usernameCandidate }, nonFatalError: 'Such an username already exists!' })
             return
           } else {
             delete user.confirm_password  // remove the useless confirm_password field because we're going to be saving to the DB
@@ -69,6 +89,14 @@ module.exports = {
   login: (req, res) => {
     let loginUser = req.body
     let usernameCandidate = loginUser.username
+    let passwordCandidate = loginUser.password
+    if (!usernameIsValid(usernameCandidate)) {
+      res.render('user/login', { logArgs: { username: usernameCandidate }, nonFatalError: 'Username can only contain letters and numbers. It should also have minimum length of 3 and maximum length of 20!' })
+      return
+    } else if (!passwordIsValid(passwordCandidate)) {
+      res.render('user/login', { logArgs: { username: usernameCandidate }, nonFatalError: 'Password can only contain letters and numbers. It also has a minimum length of 4 and a maximum length of 20!' })
+      return
+    }
     User
       .findOne({ 'username': usernameCandidate })
       .then((user) => {
